f7022edefaaad949501d416af14dcb60eb4ed51f
Doh Jun Hyeong <99169066+dohjh0219@users.noreply.github.com>
2022-03-09 00:17:35 +0900

    [Silver V] Title: 나이순 정렬, Time: 324 ms, Memory: 52656 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -10,7 +10,7 @@ user=[]
 for i in range(n):
     user.append(list(input().split()))
 
-user.sort(key=lambda a:int(a[0]))
+user.sort(key=lambda a:int(a[0])) ## age만 분석하여 sort함.
 
 for i in range(n):
     print(user[i][0], user[i][1])
